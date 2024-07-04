ca1bddcda1883b625d683c10f1eee7c20879d9d2
Clemens Banowski <officialeta09@gmail.com>
2024-07-04 20:24:04 +0200

    set cli as docker init command

--- images/controller/src/cli.js
+++ images/controller/src/cli.js
@@ -1,4 +1,7 @@
 import { CLIClient } from "@octopuscentral/controller";
 
-const client = CLIClient();
-client.start();
+const cli = new CLIClient();
+cli.on('start', () => console.info('===============================\n OctopusCentral Controller CLI \n==============================='));
+cli.on('stop', () => process.exit());
+
+cli.start();
--- images/controller/src/index.js
+++ images/controller/src/index.js
@@ -1,5 +1,7 @@
 import { Controller } from "@octopuscentral/controller";
 
+
+
 const controller = new Controller(
   process.env.SERVICE_NAME,
   process.env.DATABASE_URL,
@@ -11,4 +13,14 @@ const controller = new Controller(
 const instancesFetchInterval = Number(process.env.INSTANCES_FETCH_INTERVAL);
 if (!isNaN(instancesFetchInterval)) controller.instancesFetchInterval = instancesFetchInterval;
 
+
+
+process.on('SIGTERM', async () => {
+  console.log('stopping controller...')
+  await controller.destroy();
+  console.info('controller stopped');
+  process.exit();
+});
+
 await controller.start();
+console.info('controller started');
