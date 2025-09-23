90bf15e6ef340a4f726d2678bfdc20d605e2cbe5
Clemens Banowski <officialeta09@gmail.com>
2025-09-23 14:59:13 +0200

    code cleanup

--- images/controller/src/index.js
+++ images/controller/src/index.js
@@ -1,7 +1,6 @@
 import { Controller } from "@octopuscentral/controller";
 
 
-
 const controller = new Controller(
   process.env.SERVICE_NAME,
   process.env.DATABASE_URL,
@@ -14,13 +13,13 @@ const instancesFetchInterval = Number(process.env.INSTANCES_FETCH_INTERVAL);
 if (!isNaN(instancesFetchInterval)) controller.instancesFetchInterval = instancesFetchInterval;
 
 
-
 process.on('SIGTERM', async () => {
   console.log('stopping controller...')
   await controller.destroy();
   console.info('controller stopped');
   process.exit();
 });
+
 
 await controller.start();
 console.info('controller started');
