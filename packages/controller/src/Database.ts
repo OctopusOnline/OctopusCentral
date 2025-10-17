75c68e55dc208ee80af443e1ab911da666b6231b
Clemens Banowski <officialeta09@gmail.com>
2025-10-17 23:46:51 +0200

    removed controller db PoolConfig

--- packages/controller/src/Database.ts
+++ packages/controller/src/Database.ts
@@ -18,20 +18,9 @@ export class Database {
     this.#pool = pool;
   }
 
-  private parseUrl(url: string): PoolConfig {
-    const urlObject = new URL(url);
-    return {
-      host: urlObject.hostname,
-      port: Number(urlObject.port),
-      user: urlObject.username,
-      password: urlObject.password,
-      database: urlObject.pathname.substring(1)
-    };
-  }
-
   async connect(): Promise<void> {
     if (!this.#pool) {
-      this.#pool = createPool(this.parseUrl(this.#url));
+      this.#pool = createPool(this.#url);
       await this.testConnection();
     }
   }
